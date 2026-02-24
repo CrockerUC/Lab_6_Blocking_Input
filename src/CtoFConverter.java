e00d40cbf139f052e192c003c8c9419ad58a0ca3
CrockerUC <crockecf@mail.uc.edu>
2026-02-23 20:35:26 -0500

    Finished Task 1 (Forgot to close Scanner)

--- src/CtoFConverter.java
+++ src/CtoFConverter.java
@@ -22,5 +22,6 @@ public class CtoFConverter
         }while(!done);
         fahrenheit = (celsius * 9/5) + 32;
         System.out.println(celsius + " degrees Celsius is " + fahrenheit + " degrees Fahrenheit.");
+        in.close();
     }
 }
